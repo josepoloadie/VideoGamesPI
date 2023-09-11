f693d78ea16f18412b3fd4146e3889f676a08b43
josepoloadie <jpolo990206@gmail.com>
2023-09-10 20:41:04 -0500

    DEPLOYMENT

--- api/src/db.js
+++ api/src/db.js
@@ -1,14 +1,19 @@
 const { Sequelize } = require("sequelize");
 require("dotenv").config(); //Se requiere el archivo .env
 
-const { DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, BDD, DB_DEPLOY } = process.env; //Se importan las variables del archivo .env
+const { DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, BDD } = process.env; //Se importan las variables del archivo .env
+const DB_DEPLOY =
+  "DB_DEPLOY=postgresql://postgres:dLQJEdaVxUPFn7WHNhzl@containers-us-west-76.railway.app:7985/railway";
 const VideogameModel = require("./models/Videogame");
 const GenreModel = require("./models/Genre");
+console.log("HOLA");
 
-const sequelize = new Sequelize(
-  `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${BDD}`,
-  { logging: false, native: false }
-);
+// const sequelize = new Sequelize(
+//   `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${BDD}`,
+//   { logging: false, native: false }
+// );
+
+const sequelize = new Sequelize(DB_DEPLOY, { logging: false, native: false });
 
 //Definicion de modelos a usar
 VideogameModel(sequelize);
